def32654e098679f830d133ed301907f1f733de0
Chong <chongconan320@gmail.com>
2022-08-15 18:41:12 +0800

    Banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "components/Header";
+import { Banner } from "components/Home";
 
 function App() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 }
