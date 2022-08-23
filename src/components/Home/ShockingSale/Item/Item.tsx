4c046fc0f27de25c349890cebac5d3e432cb88a1
Chong <chongconan320@gmail.com>
2022-08-23 16:39:30 +0800

    Shocking Sales items component

--- src/components/Home/ShockingSale/Item/Item.tsx
+++ src/components/Home/ShockingSale/Item/Item.tsx
@@ -1,44 +1,52 @@
 import styles from "./item.module.css";
 import Image_ShockingSaleLabel from "assets/images/shocking_sale_label.png";
+import { AnchorHTMLAttributes, forwardRef } from "react";
 
-interface IItem {
+interface IItem extends AnchorHTMLAttributes<HTMLAnchorElement> {
   productImg: string;
   sold: number;
   total: number;
   discount: number;
+  width: number;
 }
 
-const Item = ({ productImg, sold, total, discount }: IItem) => {
-  return (
-    <div className={styles["item"]}>
-      <div className={styles["item__product"]}>
-        <img
-          src={productImg}
-          alt={"item"}
-          className={styles["product__image--product"]}
-        />
-        <img
-          src={Image_ShockingSaleLabel}
-          alt={"label"}
-          className={styles["product__image--label"]}
-        />
-        <div className={styles["product__discount"]}>
-          <span>{discount}%</span>
-          <span>OFF</span>
-        </div>
-      </div>
-      <div className={styles["item__price"]}>
-        <span>RM</span>
-        <span>11.96</span>
-      </div>
-      <div className={styles["item__progress_bar"]}>
-        <span>{sold} Sold</span>
+const Item = forwardRef<HTMLAnchorElement, IItem>(
+  ({ productImg, sold, total, discount, width, href }: IItem, ref) => {
+    return (
+      <a className={styles["item"]} href={href} ref={ref}>
         <div
-          className={styles["progress_bar__step"]}
-          style={{ width: `${(sold / total) * 100}%` }}
-        />
-      </div>
-    </div>
-  );
-};
+          className={styles["item__product"]}
+          style={{ width: `${width}px` }}
+        >
+          <img
+            src={productImg}
+            alt={"item"}
+            className={styles["product__image--product"]}
+          />
+          <img
+            src={Image_ShockingSaleLabel}
+            alt={"label"}
+            className={styles["product__image--label"]}
+          />
+          <div className={styles["product__discount"]}>
+            <span>{discount}%</span>
+            <span>OFF</span>
+          </div>
+        </div>
+        <div className={styles["item__price"]}>
+          <span>RM</span>
+          <span>11.96</span>
+        </div>
+        <div className={styles["item__progress_bar"]}>
+          <span>{sold} Sold</span>
+          <div
+            className={styles["progress_bar__step"]}
+            style={{ width: `${(sold / total) * 100}%` }}
+          />
+        </div>
+      </a>
+    );
+  }
+);
+
 export default Item;
--- src/components/Home/ShockingSale/ShockingSale.tsx
+++ src/components/Home/ShockingSale/ShockingSale.tsx
@@ -4,10 +4,77 @@ import styles from "./shockingSale.module.css";
 import Image_ShockingSale from "assets/images/shocking_sale.png";
 import Image_ShockingSaleItem from "assets/images/shocking_sale_item.png";
 
-import { ReactComponent as IconDownArrow } from "assets/icons/down-arrow.svg";
+import { ReactComponent as IconArrow } from "assets/icons/down-arrow.svg";
+
 import Item from "./Item";
+import { useEffect, useRef, useState } from "react";
+
+const ITEMS_RENDER_AMOUNT = 20;
+const AMOUNT_TO_SHOW = 5;
+const GAP = 35;
+const PADDING = 21;
 
 const ShockingSale = () => {
+  const shockingSalesRef = useRef<HTMLDivElement>(null);
+  const itemsWrapperRef = useRef<HTMLDivElement>(null);
+  const itemRef = useRef<HTMLAnchorElement>(null);
+  const [sequence, setSequence] = useState<{ [key: string]: number }>({
+    currentSelected: 1,
+    1: 0,
+  });
+  const [itemWidth, setItemWidth] = useState(0);
+  const onPreviousSequence = () => {
+    const itemsWrapperElement = itemsWrapperRef.current;
+    if (itemsWrapperElement === null) return;
+    if (sequence.currentSelected === 0) return;
+    const scrollToLeft: number = sequence[sequence.currentSelected - 1];
+    setSequence((prev) => ({
+      ...prev,
+      currentSelected: prev.currentSelected - 1,
+    }));
+    itemsWrapperElement.scroll({
+      left: scrollToLeft,
+    });
+  };
+  const onNextSequence = () => {
+    if (sequence.currentSelected * AMOUNT_TO_SHOW === ITEMS_RENDER_AMOUNT)
+      return;
+
+    const itemsWrapperElement = itemsWrapperRef.current;
+    if (itemsWrapperElement === null) return;
+
+    const scrollToLeft =
+      Math.ceil(itemWidth * AMOUNT_TO_SHOW + GAP * AMOUNT_TO_SHOW) *
+      sequence["currentSelected"];
+    itemsWrapperElement.scroll({
+      left: scrollToLeft,
+    });
+    setSequence((prev) => {
+      return {
+        ...prev,
+        currentSelected: prev.currentSelected + 1,
+        [prev.currentSelected + 1]: scrollToLeft,
+      };
+    });
+  };
+  useEffect(() => {
+    const shockingSalesElement = shockingSalesRef.current;
+    const itemsWrapperElement = itemsWrapperRef.current;
+    const itemElement = itemRef.current;
+    if (
+      itemsWrapperElement === null ||
+      itemElement === null ||
+      shockingSalesElement === null
+    )
+      return;
+    setItemWidth(
+      Math.ceil(
+        (shockingSalesElement.clientWidth - PADDING - GAP * AMOUNT_TO_SHOW) /
+          AMOUNT_TO_SHOW
+      )
+    );
+  }, []);
+
   return (
     <div className={styles["shocking_sale"]}>
       <div className={styles["shocking_sale__header"]}>
@@ -18,21 +85,53 @@ const ShockingSale = () => {
             className={styles["shocking_sale__title"]}
           />
           {/* <Countdown/> */}
-        </div>{" "}
+        </div>
         <div className={styles["header__browser_all"]}>
           <span>See All</span>
-          <IconDownArrow
-            className={styles["shocking_sale__icon--right_arrow"]}
-          />
+          <IconArrow className={styles["shocking_sale__icon--right_arrow"]} />
         </div>
       </div>
-      <div className={styles["shocking_sale__items"]}>
-        <Item
-          productImg={Image_ShockingSaleItem}
-          discount={42}
-          sold={27}
-          total={250}
-        />
+      <div className={styles["shocking_sale__items"]} ref={shockingSalesRef}>
+        {sequence.currentSelected !== 1 && (
+          <div
+            onClick={onPreviousSequence}
+            className={`${styles["shocking_sale__arrows"]} ${styles["shocking_sale__arrows--left"]}`}
+          >
+            <IconArrow
+              className={`${styles["shocking_sale__items_arrow"]} ${styles["shocking_sale__items_arrow--left"]}`}
+            />
+          </div>
+        )}
+        {sequence.currentSelected * AMOUNT_TO_SHOW !== ITEMS_RENDER_AMOUNT && (
+          <div
+            onClick={onNextSequence}
+            className={`${styles["shocking_sale__arrows"]} ${styles["shocking_sale__arrows--right"]}`}
+          >
+            <IconArrow
+              className={`${styles["shocking_sale__items_arrow"]} ${styles["shocking_sale__items_arrow--right"]} `}
+            />
+          </div>
+        )}
+
+        <div
+          className={styles["shocking_sale__items_wrapper"]}
+          ref={itemsWrapperRef}
+        >
+          {Array.from(Array(ITEMS_RENDER_AMOUNT).keys()).map((value, index) => {
+            return (
+              <Item
+                ref={itemRef}
+                key={index}
+                productImg={Image_ShockingSaleItem}
+                discount={42}
+                sold={index}
+                total={250}
+                href={"/"}
+                width={itemWidth}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
